1608a8aef649088bc01dd5a13fca2b0f519e190c
LorranRocha <youtubebizarro@hotmail.com>
2022-05-06 15:35:43 -0300

    enviando arquivos

---
+++ README.md
@@ -0,0 +1,5 @@
+### Projeto React em Desenvolvimento.
+
+### Criado com o intuito de apresentar o clima do local na qual o usuário está acessando!
+
+### Created by: Lorran Rocha url(https://www.lorranrocha.com/)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Local from "./Components/Local/Local";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-cover">
+        <Local></Local>
+      </div>
+    </div>
+  );
+}
+
+export default App;
